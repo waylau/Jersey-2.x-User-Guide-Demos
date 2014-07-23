6bdc5059745be718f534a422901b8643a8846277
waynelau <waynelau521@gmail.com>
2014-07-23 14:37:14 +0800

    add  chapter 1 source 添加第一章所有的源码

---
+++ demo-1.5/simple-heroku-webapp/src/test/java/com/example/MyResourceTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import com.example.MyResource;
+
+public class MyResourceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(MyResource.class);
+    }
+
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+    @Test
+    public void testGetIt() {
+        final String responseMsg = target().path("myresource").request().get(String.class);
+
+        assertEquals("Hello, Heroku!", responseMsg);
+    }
+}
